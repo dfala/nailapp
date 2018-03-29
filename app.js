d0bace09cf72805587ddc395965b83b69645106d
Daniel Falabella <dnlfala@gmail.com>
2018-03-29 08:37:44 -0600

    Added some imgs

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ angular.module('NailChatApp', [])
   // $scope.userMessage = 'The 2008 presidential campaign of Barack Obama, then junior United States Senator from Illinois, was announced on February 10, 2007 in Springfield, Illinois.';
 
   $scope.thread = [];
-  newBotMessage("What brings you here?")
+  newBotMessage({
+    sender: 'bot',
+    text: "What brings you here?"
+  })
 
-  function newBotMessage (text) {
+  function newBotMessage (messageObject) {
     $timeout(function () {
       $scope.loading = true;
       scrollContainer();
@@ -15,10 +18,7 @@ angular.module('NailChatApp', [])
 
     $timeout(function () {
       $scope.loading = false;
-      $scope.thread.push({
-        sender: 'bot',
-        text: text
-      });
+      $scope.thread.push(messageObject);
       scrollContainer();
     }, 2400);
   };
@@ -80,26 +80,95 @@ angular.module('NailChatApp', [])
   };
 
   var messages = [
-    "That sounds really hard.",
-    "If I were in your position, I'd feel the same way!",
-    "You're so right",
-    "Tell me more about that",
-    "What do you mean by that?",
+    {
+      sender: 'bot',
+      text: "That sounds really hard.",
+    },
+    {
+      sender: 'bot',
+      text: "If I were in your position, I'd feel the same way!",
+    },
+    {
+      sender: 'bot',
+      text: "You're so right",
+    },
+    {
+      sender: 'bot',
+      text: "Tell me more about that",
+    },
+    {
+      sender: 'bot',
+      text: "What do you mean by that?",
+    },
     // "So what you are saying is, "TEXT". Is that right?",
-    "Mm yeah. How does that make you feel?",
-    "You're so brave. How does your family feel about this?",
-    "Tell me what's worrying you",
-    "Of course you're feeling like this. I'd feel the same way",
-    "Tell me everything that’s led up to this",
-    "What set off these feelings?",
-    "What’s the thing that’s worrying you the most?",
-    "Help me undestand a little better how you're feeling about this",
-    "I can't imagine what you're going through",
-    "I am so sorry you are going through this. You are the last person on this earth who deserves that",
-    "It's not your fault",
-    "You deserve so much better",
-    ":(",
-    "I’m sorry you’re hurting so much :("
+    {
+      sender: 'bot',
+      text: "Mm yeah. How does that make you feel?",
+    },
+    {
+      sender: 'bot',
+      text: "You're so brave. How does your family feel about this?",
+    },
+    {
+      sender: 'bot',
+      text: "Tell me what's worrying you",
+    },
+    {
+      sender: 'bot',
+      text: "Of course you're feeling like this. I'd feel the same way",
+    },
+    {
+      sender: 'bot',
+      text: "Tell me everything that’s led up to this",
+    },
+    {
+      sender: 'bot',
+      text: "What set off these feelings?",
+    },
+    {
+      sender: 'bot',
+      text: "What’s the thing that’s worrying you the most?",
+    },
+    {
+      sender: 'bot',
+      text: "Help me undestand a little better how you're feeling about this",
+    },
+    {
+      sender: 'bot',
+      text: "I can't imagine what you're going through",
+    },
+    {
+      sender: 'bot',
+      text: "I am so sorry you are going through this. You are the last person on this earth who deserves that",
+    },
+    {
+      sender: 'bot',
+      text: "It's not your fault",
+    },
+    {
+      sender: 'bot',
+      text: "You deserve so much better",
+    },
+    {
+      sender: 'bot',
+      text: ":(",
+    },
+    {
+      sender: 'bot',
+      text: "I’m sorry you’re hurting so much :(",
+    },
+    {
+      sender: 'bot',
+      img: 'https://media.giphy.com/media/2WxWfiavndgcM/giphy.gif'
+    },
+    {
+      sender: 'bot',
+      img: 'https://media.giphy.com/media/Ty9Sg8oHghPWg/giphy.gif'
+    },
+    {
+      sender: 'bot',
+      img: 'https://media.giphy.com/media/MuztdWJQ4PR7i/giphy.gif'
+    }
   ];
 
   return service;
